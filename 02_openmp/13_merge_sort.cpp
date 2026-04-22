839f3348e91755b5f75f6b8e8c27b53b4831ed6b
muriiSan <mahmoud.murish@gmail.com>
2026-04-22 15:57:56 +0900

    Assignment 1

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -16,15 +16,20 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else
       tmp[i] = vec[right++]; 
   }
-  for (int i=0; i<tmp.size(); i++) 
-    vec[begin++] = tmp[i];
+
+  for (int i=0; i<(int)tmp.size(); i++)
+    vec[begin+i] = tmp[i];
 }
 
+// parallelize the recursive function 
 void merge_sort(std::vector<int>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+  #pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+  #pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+  #pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
@@ -32,11 +37,14 @@ void merge_sort(std::vector<int>& vec, int begin, int end) {
 int main() {
   int n = 20;
   std::vector<int> vec(n);
+#pragma omp parallel for
   for (int i=0; i<n; i++) {
     vec[i] = rand() % (10 * n);
     printf("%d ",vec[i]);
   }
+
   printf("\n");
+#pragma omp single
   merge_sort(vec, 0, n-1);
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
