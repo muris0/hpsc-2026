9a73b204857a84ec9524d727b4574fd3a2a71edf
un07187 <mahmoud.murish@gmail.com>
2026-05-14 02:04:12 +0900

    Assignment - Lecture  6

---
+++ 06_cuda/13_bucket_sort.cu
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+__global__ void countKernel(int* key, int* bucket, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n)
+        atomicAdd(&bucket[key[i]], 1);
+}
+
+int main() {
+    int n = 50;
+    int range = 5;
+
+    int *key, *bucket;
+    cudaMallocManaged(&key,    n     * sizeof(int));
+    cudaMallocManaged(&bucket, range * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        key[i] = rand() % range;
+        printf("%d ", key[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < range; i++)
+        bucket[i] = 0;
+
+    countKernel<<<(n + 255) / 256, 256>>>(key, bucket, n);
+    cudaDeviceSynchronize();
+
+    for (int i = 0, j = 0; i < range; i++)
+        for (; bucket[i] > 0; bucket[i]--)
+            key[j++] = i;
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", key[i]);
+    printf("\n");
+
+    cudaFree(key);
+    cudaFree(bucket);
+}
