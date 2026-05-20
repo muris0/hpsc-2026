bfff3d9cf5ff70c079362ff2ca1900368b91d91f
un07187 <mahmoud.murish@gmail.com>
2026-05-20 21:44:09 +0900

    Assignment - Lecture 9

--- 09_io/16_phdf5_write.cpp
+++ 09_io/16_phdf5_write.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main (int argc, char** argv) {
-  const int NX = 10000, NY = 10000;
+  const int NX = 1000, NY = 1000;
   hsize_t dim[2] = {2, 2};
   int mpisize, mpirank;
   MPI_Init(&argc, &argv);
@@ -15,10 +15,11 @@ int main (int argc, char** argv) {
   assert(mpisize == dim[0]*dim[1]);
   hsize_t N[2] = {NX, NY};
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
-  hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  hsize_t offset[2] = {(hsize_t)(mpirank / dim[0]),(hsize_t)( mpirank % dim[0])};
+  hsize_t count[2] = {Nlocal[0],Nlocal[1]};
+  hsize_t stride[2] = {dim[0],dim[1]};
+  hsize_t block[2] = {1, 1};
+  
   vector<int> buffer(Nlocal[0]*Nlocal[1],mpirank);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
@@ -27,7 +28,7 @@ int main (int argc, char** argv) {
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
   hid_t dataset = H5Dcreate(file, "dataset", H5T_NATIVE_INT, globalspace,
 			    H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
